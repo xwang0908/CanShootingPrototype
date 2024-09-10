141710592a9caf022a218a9175471b8c84059f27
e-sawyer <109112191+ewsawyer@users.noreply.github.com>
2024-09-09 18:23:52 -0700

    refactor cans and can spawning

--- Assets/Scripts/CanMovement.cs
+++ Assets/Scripts/CanMovement.cs
@@ -21,24 +21,11 @@ public class CanMovement : MonoBehaviour
         rb.AddForce(new Vector2(Random.Range(-50f, 50f), launchForce)); // Randomize horizontal force for a varied effect
     }*/
 
-    public float launchForce = 800f;   // Adjust this value for the strength of the throw
     public int maxHits = 3;            // Maximum hits before the can is destroyed
     private int currentHits = 0;       // Track how many times the can has been hit
 
     private Rigidbody2D rb;
-
-    void Start()
-    {
-        // Set a random starting position for the can
-        transform.position = new Vector2(Random.Range(-8f, 8f), -5f);
-
-        // Get the Rigidbody2D component attached to the can
-        rb = GetComponent<Rigidbody2D>();
-
-        // Apply an upward force to "throw" the can
-        rb.AddForce(new Vector2(Random.Range(-50f, 50f), launchForce)); // Randomize horizontal force for a varied effect
-    }
-
+    
     // Method to handle being hit by a shot
     public void Hit()
     {
--- Assets/Scripts/CanSpawner.cs
+++ Assets/Scripts/CanSpawner.cs
@@ -7,16 +7,34 @@ public class CanSpawner : MonoBehaviour
 
     [Tooltip("The prefab to instantiate when spawning new cans")] [SerializeField]
     private GameObject CanPrefab;
+
+    [Tooltip("The initial speed to launch the can at")] [SerializeField]
+    private float LaunchSpeed;
+
+    [Tooltip("Possible variation in launch angle as the component of total launch speed along the spawner's x-axis")]
+    [SerializeField]
+    private float HorizontalLaunchVariation;
     
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            SpawnCan();
+    }
+
+    public void SpawnCan()
+    {
+        // Create a new can
+        GameObject can = Instantiate(CanPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D rb = can.GetComponent<Rigidbody2D>();
+
+        // Launch it into frame
+        float variation = Random.Range(-HorizontalLaunchVariation, HorizontalLaunchVariation);
+        rb.AddForce(LaunchSpeed * rb.mass * (transform.up + variation * transform.right), ForceMode2D.Impulse);
     }
 }
